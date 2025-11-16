afe9a3650655c62457999368e76a612249616d6d
Dairo Mosquera <dairoandres115@outlook.com>
2025-11-16 18:57:30 -0500

    feat: Implement data fetching and error handling functions
    Added functions to fetch data with simulated success and error handling using Result and Option types. Included type narrowing and pattern matching examples for handling results.

---
+++ example.py
@@ -0,0 +1,36 @@
+import random
+
+from simple_result import Ok, Err, UnwrapError, Result, ResultOption
+
+
+def fetch_data() -> Result[str, ConnectionError]:
+    fetched = random.choice([True, False])
+    if fetched:
+        return Ok('Data fetched!')
+    return Err(ConnectionError('Error fetching data!'))
+
+
+def using_type_narrowing() -> None:
+    print('Using type narrowing')
+    if res := fetch_data():
+        print(res.unwrap_value()) # "Data fetched!"
+        assert res.value == 'Data fetched!'
+        assert res.error is None
+    else:
+        print(res.unwrap_error()) # "Error fetching data!"
+        assert res.value is None
+        assert str(res.error) == 'Error fetching data!'
+
+
+def using_match() -> None:
+    print('Using match')
+    match fetch_data():
+        case Ok(data):
+            print(data) # "Data fetched!"
+        case Err(error):
+            print(error) # "Error fetching data!"
+
+
+if __name__ == '__main__':
+    using_type_narrowing()
+    using_match()
